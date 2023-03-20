20945f80a33fc16bea43eeaa9ca1ffec0a1c4ae5
Oscar Esteban Pacheco Cabrera <l22211628@tectijuana.edu.mx>
2023-03-19 18:08:30 -0700

    Se agrego en la clase pelicula los campos: -titulo -anio -pais -director; se crearon dos objetos peliculas, se les agrago titulo y anio; estos datos se imprimen en la consola.

--- Pelicula/Program.cs
+++ Pelicula/Program.cs
@@ -7,6 +7,10 @@ namespace Pelicula
     public class Pelicula
     {
         //Campos
+        public string titulo;
+        public int año;
+        public string pais;
+        public string director;
         private List<Actor> actores = new List<Actor>();
 
         //Constructores
@@ -32,7 +36,7 @@ namespace Pelicula
         //Métodos 
         public void Imprime()
         {
-            Console.WriteLine($"{Nombre} ({Año})");
+      //      Console.WriteLine($"{Nombre} ({Año})");
         }
     }
 
@@ -45,6 +49,17 @@ namespace Pelicula
 
         static void Main(string[] args)
         {
+            Pelicula pelicula1 = new Pelicula();
+            Pelicula pelicula2 = new Pelicula();
+            pelicula1.titulo = "El señor de los anillos: La comunidad del anillo";
+            pelicula1.año = 2001;
+            pelicula2.titulo = "El señor de los anillos: Las dos torres";
+            pelicula2.año = 2002;
+            Console.WriteLine(pelicula1.titulo);
+            Console.WriteLine(pelicula1.año);
+            Console.WriteLine(pelicula2.titulo);
+            Console.WriteLine(pelicula2.año);
+
             Console.WriteLine("Hello");
         }
     }
